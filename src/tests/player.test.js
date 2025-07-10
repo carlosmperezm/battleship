65858b75b3e2bbaafacbf611fc01b08d1c3f1656
CarlosPerez <carlosmperez2901@gmail.com>
2025-07-10 15:29:29 -0700

    Add player tests

---
+++ src/tests/player.test.js
@@ -0,0 +1,15 @@
+import { Player } from '../models/Player.js';
+
+describe('Players Gameboards Creation tests', () => {
+  test('Players have diferent gameboards', () => {
+    const player1 = new Player([1, 2]);
+    const player2 = new Player([1, 2]);
+    const player3 = new Player([1, 2]);
+    const player4 = new Player([1, 2]);
+
+    expect(player1.gameboard).not.toBe(player2.gameboard);
+    expect(player2.gameboard).not.toBe(player3.gameboard);
+    expect(player3.gameboard).not.toBe(player4.gameboard);
+    expect(player4.gameboard).not.toBe(player1.gameboard);
+  });
+});
